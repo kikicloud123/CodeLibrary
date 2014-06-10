fbc3d9f9fb6a91013a3b1bbaaa8afb2ab2f2e74e
liuquanwei <lanou3g@weiwei.local>
2014-06-10 16:45:42 +0800

    2
    更改页面背景

--- 2014-6-10github/2014-6-10github/LZAppDelegate.m
+++ 2014-6-10github/2014-6-10github/LZAppDelegate.m
@@ -14,8 +14,9 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
+    self.window.backgroundColor = [UIColor blackColor];
     [self.window makeKeyAndVisible];
+
     return YES;
 }
 
