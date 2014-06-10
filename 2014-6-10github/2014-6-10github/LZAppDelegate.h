b96e4f9c6a4d2f08608704b1ef9bc54fa3c8249f
liuquanwei <lanou3g@weiwei.local>
2014-06-10 16:39:11 +0800

    1.0
    项目第一天

---
+++ 2014-6-10github/2014-6-10github/LZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LZAppDelegate.h
+//  2014-6-10github
+//
+//  Created by lanou3g on 14-6-10.
+//  Copyright (c) 2014年 lanou3g.com 蓝欧科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 2014-6-10github/2014-6-10github/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  2014-6-10github
+//
+//  Created by lanou3g on 14-6-10.
+//  Copyright (c) 2014年 lanou3g.com 蓝欧科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LZAppDelegate class]));
+    }
+}
